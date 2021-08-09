805da4e499bd009336f4ef1bb1b90911c44cbfb4
Coderkhayrul <coderkhayrul>
2021-08-10 00:47:59 +0600

    Create API Controller

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    // REGISTER METHORD - POST
+    public function register(Request $request)
+    {
+    }
+
+    // LOGIN METHORD - POST
+    public function lgoin(Request $request)
+    {
+    }
+
+    // PROFILE METHORD - GET
+    public function profile()
+    {
+    }
+
+    // LOGOUT METHORD - GET
+    public function logout()
+    {
+    }
+}
---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // CREATE METHORD - POST
+    public function createBook(Request $request)
+    {
+    }
+
+    // LIST METHORD - GET
+    public function listBook()
+    {
+    }
+
+    // SINGLE BOOKS - GET
+    public function singleBook($book_id)
+    {
+    }
+
+    // UPDATE METHORD - PUT
+    public function updateBook(Request $request, $book_id)
+    {
+    }
+
+    // DELETE METHORD - GET
+    public function deleteBook($book_id)
+    {
+    }
+}
