5a6373c3cfc5fdcccf9a100015b456fa8f852da9
Coderkhayrul <coderkhayrul>
2021-08-10 00:37:36 +0600

    Create API Model and Migration

---
+++ database/migrations/2021_08_09_182920_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 120);
+            $table->string('email', 120);
+            $table->string('password', 120);
+            $table->string('phone_no', 30);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
